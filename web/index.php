70041bf38bf0cdbd612f21c70288f93cb3655eaa
Hermione See <20019634@myrp.edu.sg>
2022-07-08 12:18:11 +0800

    updates

--- web/index.php
+++ web/index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="summary" content="Created for C339 Lesson 8">
     <title>Hello World!</title>
 </head>
 <body>
