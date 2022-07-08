41be3fb92c95ff7d9fdf1f10fb2b1482b7e69e64
Hermione See <20019634@myrp.edu.sg>
2022-07-08 12:45:23 +0800

    done

--- web/index.php
+++ web/index.php
@@ -1,11 +1,11 @@
-<!-- fix : <html> element does not have a [lang] attribute -->
-<!-- <!DOCTYPE html> -->
+<!-- fix : Page lacks the HTML doctype, thus triggering quirks mode -->
+<!DOCTYPE html>
 <!-- fix : <html> element does not have a [lang] attribute -->
 <html lang="en">
 <head>
     <!-- fix : Does not have a <meta name="viewport"> tag with width or initial-scale -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- fix : <html> element does not have a [lang] attribute -->
+    <!-- fix : Document does not have a meta description -->
     <meta name="description" content="Created for C339 Lesson 8">
     <!-- fix : Document doesn't have a <title> element -->
     <title>Hello World!</title>
