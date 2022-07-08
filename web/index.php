010505203c2c1b4ef1c98e6b85038773f9877457
Hermione See <20019634@myrp.edu.sg>
2022-07-08 12:43:15 +0800

    !DOCTYPE

--- web/index.php
+++ web/index.php
@@ -1,8 +1,13 @@
-<!DOCTYPE html>
+<!-- fix : <html> element does not have a [lang] attribute -->
+<!-- <!DOCTYPE html> -->
+<!-- fix : <html> element does not have a [lang] attribute -->
 <html lang="en">
 <head>
+    <!-- fix : Does not have a <meta name="viewport"> tag with width or initial-scale -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- fix : <html> element does not have a [lang] attribute -->
     <meta name="description" content="Created for C339 Lesson 8">
+    <!-- fix : Document doesn't have a <title> element -->
     <title>Hello World!</title>
 </head>
 <body>
