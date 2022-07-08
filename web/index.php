4194db252197c6320569b0b43b8eba0c6d8ba84a
Hermione See <20019634@myrp.edu.sg>
2022-07-08 12:15:15 +0800

    updated

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
-<meta name="viewport">
 <html lang="en">
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hello World!</title>
 </head>
 <body>
